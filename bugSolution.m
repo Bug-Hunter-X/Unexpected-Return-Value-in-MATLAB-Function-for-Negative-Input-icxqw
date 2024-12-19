fbaddda4a3913b683651b0c423773c9e91a2de98
BugHunter-X <askzenai@gmail.com>
2024-12-19 15:20:08 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,12 @@
+function result = myFunction(input)
+  % Some code here...
+  if input < 0
+    result = -1; % Handle negative input
+  else
+    % More code here...
+  end
+end
+
+% Example usage
+input = -5;
+result = myFunction(input);
